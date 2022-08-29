f0be59411d4ef17875cd550f9c4ed8255def03d8
Noah Harris <noahharris@dhcp-172-24-59-201.utsarr.net>
2022-08-29 09:42:03 -0500

    Heroku changes

--- src/app.js
+++ src/app.js
@@ -14,7 +14,7 @@ const app = express()
 
 // Middleware
 app.use(express.json())
-//app.use(express.urlencoded( {extended: false}))
+app.use(express.urlencoded( {extended: false}))
 
 
 // Load Routers
