e92f0fc4fafa74680efdf60e3fc1f770521e9df4
Noah Harris <noahharris@Noahs-MacBook-Pro-4.local>
2022-09-06 10:01:19 -0500

    Cors Configuration

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const dotenv = require('dotenv')
 dotenv.config()
 const connectDB = require('./config/connectDB')
@@ -15,6 +16,7 @@ const app = express()
 // Middleware
 app.use(express.json())
 app.use(express.urlencoded( {extended: false}))
+app.use(cors())
 
 
 // Load Routers
